1d3e4404e026043ff7a0790884f20ba004a22bf6
İrem Özyılmaz <iozyilmaz8@gmail.com>
2025-03-12 11:25:39 +0300

    Create kullanıcı.py

---
+++ kullanıcı.py
@@ -0,0 +1,7 @@
+# Kullanıcıdan giriş alınır
+ad = input("Adınızı giriniz: ")
+soyad = input("Soyadınızı giriniz: ")
+yas = input("Yaşınızı giriniz: ")
+
+# Sonuç ekrana yazdırılır
+print(f"Merhaba {ad} {soyad}, {yas} yaşındasınız. Hoş geldiniz!")
